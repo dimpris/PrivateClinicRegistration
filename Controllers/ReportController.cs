86bc88f5224a4e6fe7455244241714fd51365266
Dmytro Prysiazhnyi <dimpris@gmail.com>
2020-10-16 08:57:57 +0300

    Added report controller with views

---
+++ Controllers/ReportController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PrivateClinicRegistration.Controllers
+{
+    public class ReportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Report1()
+        {
+            return View();
+        }
+
+        public IActionResult Report2()
+        {
+            return View();
+        }
+    }
+}
+
+/*
+ SELECT Profile, Fullname, COUNT(*) as Cnt
+FROM Appointment
+JOIN Doctor ON DoctorID = Doctor.ID
+GROUP BY Profile, Fullname
+ORDER BY Cnt DESC
+
+SELECT MONTH(DateTimeStart) as AppointmentMonth, COUNT(*) as Cnt
+FROM Appointment
+GROUP BY MONTH(DateTimeStart)
+ORDER BY Cnt DESC
+ */
